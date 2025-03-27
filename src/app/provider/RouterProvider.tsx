36de46299f383aa0a0aa98d88c83d9b0093efeae
eunhak <akfmzh1207@naver.com>
2025-03-28 00:45:51 +0900

    feat/#7: 카테고리 레이아웃 적용 (중첩라우팅)

--- src/app/provider/RouterProvider.tsx
+++ src/app/provider/RouterProvider.tsx
@@ -1,11 +1,24 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from '@/pages/Home';
+import CategoryLayout from '@/app/layouts/CategoryLayout';
+import ChargePage from '@/pages/ChargePage';
+import ChartPage from '@/pages/ChartPage';
+import EventPage from '@/pages/EventPage';
+import NewsPage from '@/pages/NewsPage';
+import StorePage from '@/pages/StorePage';
+import WhookPage from '@/pages/WhookPage';
 
 const RouterProvider = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<CategoryLayout />}>
+          <Route path="/" element={<ChartPage />} />
+          <Route path="/whook" element={<WhookPage />} />
+          <Route path="/store" element={<StorePage />} />
+          <Route path="/charge" element={<ChargePage />} />
+          <Route path="/event" element={<EventPage />} />
+          <Route path="/news" element={<NewsPage />} />
+        </Route>
       </Routes>
     </Router>
   );
