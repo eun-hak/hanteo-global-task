0f51f7296173f3c49d0e04837cd9d8da52901bc0
eunhak <akfmzh1207@naver.com>
2025-03-28 00:45:25 +0900

    feat/#7: CategoryTabs 로직 구현

---
+++ src/widgets/CategoryTabs/constants.ts
@@ -0,0 +1,8 @@
+export const CATEGORIES = [
+  { label: '차트', path: '/' },
+  { label: 'Whook', path: '/whook' },
+  { label: '이벤트', path: '/event' },
+  { label: '뉴스', path: '/news' },
+  { label: '스토어', path: '/store' },
+  { label: '충전소', path: '/charge' },
+] as const;
