8e8c1db3474618d7bf3411915cad6bdeaf9b312e
eunhak <akfmzh1207@naver.com>
2025-03-27 16:20:19 +0900

    chore/#3: font + theme + typo 설정

---
+++ src/app/provider/EmotionProvider.tsx
@@ -0,0 +1,46 @@
+import { css, Global, ThemeProvider } from '@emotion/react';
+import theme from '../styles/theme';
+
+const globalStyles = css`
+  body {
+    margin: 0;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/font/Pretendard-Regular.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/font/Pretendard-Medium.woff2') format('woff2');
+    font-weight: 500;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/font/Pretendard-SemiBold.woff2') format('woff2');
+    font-weight: 600;
+    font-style: bold;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/font/Pretendard-Bold.woff2') format('woff2');
+    font-weight: 700;
+    font-style: bold;
+    font-display: swap;
+  }
+`;
+
+const EmotionProvider = ({ children }: { children: React.ReactNode }) => (
+  <ThemeProvider theme={theme}>
+    <Global styles={globalStyles} />
+    <div>{children}</div>
+  </ThemeProvider>
+);
+
+export default EmotionProvider;
