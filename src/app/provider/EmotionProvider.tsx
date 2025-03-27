cbb500731a936c7c216c54c3826382a69f2fa3fa
eunhak <akfmzh1207@naver.com>
2025-03-27 17:17:45 +0900

    fix/#3: fonts 추상화

--- src/app/provider/EmotionProvider.tsx
+++ src/app/provider/EmotionProvider.tsx
@@ -1,39 +1,12 @@
 import { css, Global, ThemeProvider } from '@emotion/react';
+import { fontFaces } from '../styles/fonts';
 import theme from '../styles/theme';
 
 const globalStyles = css`
   body {
     margin: 0;
   }
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/font/Pretendard-Regular.woff2') format('woff2');
-    font-weight: 400;
-    font-style: normal;
-    font-display: swap;
-  }
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/font/Pretendard-Medium.woff2') format('woff2');
-    font-weight: 500;
-    font-style: normal;
-    font-display: swap;
-  }
-
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/font/Pretendard-SemiBold.woff2') format('woff2');
-    font-weight: 600;
-    font-style: normal;
-    font-display: swap;
-  }
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/font/Pretendard-Bold.woff2') format('woff2');
-    font-weight: 700;
-    font-style: normal;
-    font-display: swap;
-  }
+  ${fontFaces}
 `;
 
 const EmotionProvider = ({ children }: { children: React.ReactNode }) => (
---
+++ src/app/styles/fonts.ts
@@ -0,0 +1,32 @@
+import { css } from '@emotion/react';
+
+export const fontFaces = css`
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/font/Pretendard-Regular.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/font/Pretendard-Medium.woff2') format('woff2');
+    font-weight: 500;
+    font-style: normal;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/font/Pretendard-SemiBold.woff2') format('woff2');
+    font-weight: 600;
+    font-style: normal;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/font/Pretendard-Bold.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+    font-display: swap;
+  }
+`;
