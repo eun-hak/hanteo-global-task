b15f9e15ac698cdb06488f0d58390602eda64e7c
eunhak <akfmzh1207@naver.com>
2025-03-28 18:49:52 +0900

    fix/#11: Router 구조 수정

--- src/app/provider/RouterProvider.tsx
+++ src/app/provider/RouterProvider.tsx
@@ -1,25 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import CategoryLayout from '@/app/layouts/CategoryLayout';
-import ChargePage from '@/pages/ChargePage';
-import ChartPage from '@/pages/ChartPage';
-import EventPage from '@/pages/EventPage';
-import NewsPage from '@/pages/NewsPage';
-import StorePage from '@/pages/StorePage';
-import WhookPage from '@/pages/WhookPage';
+import HomePage from '@/pages/HomePage';
 import { ROUTES } from '@/shared/config/routes';
 
 const RouterProvider = () => {
   return (
     <Router>
       <Routes>
-        <Route path={ROUTES.CHART} element={<CategoryLayout />}>
-          <Route index element={<ChartPage />} />
-          <Route path={ROUTES.WHOOK} element={<WhookPage />} />
-          <Route path={ROUTES.STORE} element={<StorePage />} />
-          <Route path={ROUTES.CHARGE} element={<ChargePage />} />
-          <Route path={ROUTES.EVENT} element={<EventPage />} />
-          <Route path={ROUTES.NEWS} element={<NewsPage />} />
-        </Route>
+        <Route path={ROUTES.HOME} element={<HomePage />} />
       </Routes>
     </Router>
   );
