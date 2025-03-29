4f0f307ecd6cdb1dedce089049dc467dd7056a41
eunhak <akfmzh1207@naver.com>
2025-03-29 17:18:03 +0900

    feat/#13: 무한스크롤 뷰로직 구현

---
+++ src/features/hanteo/ui/HanteoList.tsx
@@ -0,0 +1,70 @@
+import styled from '@emotion/styled';
+import { Virtuoso } from 'react-virtuoso';
+import { useHanteoListController } from '../model/useHanteoListController';
+import HanteoSkeleton from './HanteoSkeleton';
+import ListItem from './ListItem';
+
+const HanteoList = () => {
+  const { flatData, loadMore, isLoading, isFetchingNextPage } = useHanteoListController();
+
+  return (
+    <div>
+      <VirtuosoWrapper>
+        <ContentName>차트 목록</ContentName>
+        {isLoading && Array.from({ length: 8 }).map((_, i) => <HanteoSkeleton key={i} />)}
+        <Virtuoso
+          style={{ height: '100%' }}
+          data={flatData}
+          components={{
+            Scroller: CustomScroller,
+            Footer: () =>
+              isFetchingNextPage ? (
+                <Footer>
+                  <Spinner />
+                </Footer>
+              ) : null,
+          }}
+          endReached={loadMore}
+          itemContent={(index, item) => <ListItem key={item.title} item={item} />}
+        />
+      </VirtuosoWrapper>
+    </div>
+  );
+};
+
+export default HanteoList;
+
+const VirtuosoWrapper = styled.div`
+  height: calc(100vh - 300px);
+  padding-bottom: 40px;
+`;
+
+const ContentName = styled.div`
+  padding: 12px;
+  font-weight: 700;
+`;
+const CustomScroller = styled.div`
+  scrollbar-width: none;
+`;
+
+const Footer = styled.div`
+  padding: 10px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Spinner = styled.div`
+  width: 25px;
+  height: 25px;
+  border: 3px solid #ccc;
+  border-top: 3px solid #333;
+  border-radius: 50%;
+  animation: spin 0.8s linear infinite;
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
